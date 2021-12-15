d19b83cc7c9bae467c7bd074ed7c523b15c7b956
Tesfa-Worku <hopegorg@gmail.com>
2021-12-14 22:34:23 -0500

    feat(send): add method to send message #13

--- src/Components/GreetingCard.js
+++ src/Components/GreetingCard.js
@@ -8,6 +8,26 @@ function GreetingCard({ email, greeting, body, closing, setCustomize, preview, s
         setPreview(false);
     }
 
+    const sendEcard = () => {
+        let mailto = `mailto:${email}`;
+            mailto += '?subject=E-Card';
+            mailto += '&body=You have received an e-greeting card.';
+            mailto += '%0D%0A%0D%0A';
+            mailto += 'Go To Greeting Card';
+            mailto += '%0D%0A';
+    
+        let url = 'https://tesfa-worku.github.io/ecard';
+            url += `?greeting=${encodeURIComponent(greeting)}`;
+            url += `&body=${encodeURIComponent(body)}`;
+            url += `&closing=${encodeURIComponent(closing)}`;
+    
+            mailto += encodeURIComponent(url);
+    
+            console.log(mailto);
+    
+        window.open(mailto);
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -17,7 +37,7 @@ function GreetingCard({ email, greeting, body, closing, setCustomize, preview, s
                 <p>{closing}</p>
                 {preview
                     ? <div>
-                        <button onClick={continueEditing}>Continue Editing</button>                        
+                        <button onClick={continueEditing}>Continue Editing</button><br />
                         <button onClick={sendEcard}>Send</button>                        
                     </div>                    
                     : <button className="App-link" onClick={() => setCustomize(true)}>Reshare This card</button>
