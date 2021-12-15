7de1fd135071635bbff49b6ddef63f5e2d02b812
Tesfa-Worku <hopegorg@gmail.com>
2021-12-14 20:27:50 -0500

    feat(preview): add preview functionality #10

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import CustomizeMessage from './Components/CustomizeMessage';
 import { useState} from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
   const [greeting, setGreeting] = useState('Dear loved one');
   const [body, setBody] = useState('have a wonderful and joyful holiday');
   const [closing, setClosing] = useState('yours, with Love');
   const [customize, setCustomize] = useState(false);
+  const [preview, setPreview] = useState(true);
+
   return (
     <>
       {customize
-        ?<CustomizeMessage 
+        ? <CustomizeMessage
+          email={email}
+          setEmail={setEmail}
           greeting={greeting}
+          setGreeting={setGreeting}
+          body={body}
+          setBody={setBody}
+          closing={closing}
+          setClosing={setClosing}
+          setPreview={setPreview}
+          setCustomize={setCustomize}
         />
-        :<GreetingCard 
+        : <GreetingCard
+          email={email}
           greeting={greeting}
           body={body}
           closing={closing}
           setCustomize={setCustomize}
+          preview={preview}
+          setPreview={setPreview}
         />
       }
     </>
--- src/Components/CustomizeMessage.js
+++ src/Components/CustomizeMessage.js
@@ -2,9 +2,7 @@ import HolidayImage from '../Images/HolidayImage.jpg';
 import '../App.css';
 import { useState} from 'react';
 
-function CustomizeMessage({ greeting, body, closing, setGreeting, setBody, setClosing, setPreview, setCustomize}) {
-    const [newMessage, setNewMessage] = useState('');
-    const [email, setEmail] = useState('');
+function CustomizeMessage({ email, greeting, body, closing, setGreeting, setBody, setClosing, setEmail, setPreview, setCustomize}) {    
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -21,57 +19,58 @@ function CustomizeMessage({ greeting, body, closing, setGreeting, setBody, setCl
                     Write your own custom message below
                 </p>
             </div>
-        <form>
-            <div>
-                <label htmlFor="email">Recipient Email</label><br />
-                <input
-                    name="email"
-                    id="email"
-                    type="text"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    placeholder="Recipient Email"
-                />
-            </div>
 
-            <div>
-                <label htmlFor="greeting">Greeting</label><br />
-                <input
-                    name="greeting"
-                    id="greeting"
-                    type="text"
-                    value={greeting}
-                    onChange={(event) => setGreeting(event.target.value)}
-                    placeholder="Greeting"
-                />
-            </div>
+            <form>
+                <div>
+                    <label htmlFor="email">Recipient Email</label><br />
+                    <input
+                        name="email"
+                        id="email"
+                        type="text"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        placeholder="Recipient Email"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="body">Body</label><br />
-                <textarea
-                    name="body"
-                    id="body"
-                    value={body}
-                    onChange={event => setBody(event.target.value)}
-                    placeholder="Body"
-                    rows="3"
-                >
-                </textarea>
-            </div>
+                <div>
+                    <label htmlFor="greeting">Greeting</label><br />
+                    <input
+                        name="greeting"
+                        id="greeting"
+                        type="text"
+                        value={greeting}
+                        onChange={(event) => setGreeting(event.target.value)}
+                        placeholder="Greeting"
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="closing">Closing</label><br />
-                <input
-                    name="closing"
-                    id="closing"
-                    type="text"
-                    value={closing}
-                    onChange={event => setClosing(event.target.value)}
-                    placeholder="Closing"
-                />
-            </div>
+                <div>
+                    <label htmlFor="body">Body</label><br />
+                    <textarea
+                        name="body"
+                        id="body"
+                        value={body}
+                        onChange={event => setBody(event.target.value)}
+                        placeholder="Body"
+                        rows="3"
+                    >
+                    </textarea>
+                </div>
 
-        <button type="submit" onClick={onSubmit}>Preview Greeting Card</button>            
+                <div>
+                    <label htmlFor="closing">Closing</label><br />
+                    <input
+                        name="closing"
+                        id="closing"
+                        type="text"
+                        value={closing}
+                        onChange={event => setClosing(event.target.value)}
+                        placeholder="Closing"
+                    />
+                </div>
+
+                <button type="submit" onClick={onSubmit}>Preview Greeting Card</button>            
             </form>
         </div>        
     )
